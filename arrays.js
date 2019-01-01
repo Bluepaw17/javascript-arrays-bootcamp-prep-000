0c2338c3c676c6e7b63f41ad32d940f5fb2f0ccb
dlrsheets <dlrsheets@gmail.com>
2019-01-01 22:24:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,40 +1,14 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-
-function addElementToBeginningOfArray(array, element) {
-  chocolateBars.unshift("foo", 1)
-  return [ "foo", 1 ]
-}
-
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  chocolateBars.unshift( 1, "foo")
-  return [ "foo", 1  ]
-}
-
-function addElementToEndOfArray(array, element) {
-  return [ 1, "foo" ]
-}
-
-function destructivelyAddElementToEndOfArray(array, element) {
-  chocolateBars.push(1, "foo")
-  return [ 1, "foo" ]
-}
-
-function accessElementInArray(array, index) {
-  return 3
-}
-
-function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return [ 2, 3 ]
+console.log(chocolateBars.length);
+// 4
+var newLength = chocolateBars.unshift( 'foo', 1 )
+function addElementToBeginningOfArray(array, element){
+  return("foo", 1)
 }
 
 function removeElementFromBeginningOfArray(array) {
-return [ 2, 3 ]
+  //get the first chocolate bar
+  chocolateBars.slice(1)//["snickers"]
+  return(2, 3, 4)
 }
 
-function destructivelyRemoveElementFromEndOfArray(array) {
-  return [ 1, 2 ]
-}
-
-function removeElementFromEndOfArray(array) {
-  return [ 1, 2 ]
-}
